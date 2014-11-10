889475a5ae0c93ba1632acfaf0e12705d538d4cd
Chris Gill <chrisfgill@gmail.com>
2014-11-10 15:45:27 -0500

    reworked as a factory, cleaned up

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,72 @@
+use std::comm::{channel,SyncSender,Sender};
+
+pub enum DoTask {
+    Quit,
+    Work(proc():Send),
+}
+
+struct Worker;
+impl Worker {
+    pub fn new (tx:SyncSender<SyncSender<DoTask>>) {
+        let tb = std::task::TaskBuilder::new();
+        let tb = tb.named("worker");
+        tb.spawn(proc() {
+            let (wx, wr) = sync_channel::<DoTask>(1);
+            tx.send(wx.clone()); //send taskman our worker chan
+
+            loop { //wait on work
+                match wr.recv() {
+                    Work(f) => {
+                        f();
+                        tx.send(wx.clone())
+                    }
+                    _ => break,
+                }
+            }
+        });
+    }
+}
+
+pub struct TaskMan {
+    tx:SyncSender<SyncSender<DoTask>>, //task sender
+    fx:Sender<DoTask>, //func sender
+}
+
+impl TaskMan {
+    pub fn new (n:u8) -> TaskMan {// fr:Receiver<DoTask>, Sender<Sender<DoTask>>
+        let (fx, fr) = channel::<DoTask>();
+        let (tx,tr) = sync_channel::<SyncSender<DoTask>>(0);
+        let tb = std::task::TaskBuilder::new();
+        let tb = tb.named("taskman");
+        tb.spawn(proc() {
+            loop {
+                match fr.recv() {
+                    Quit => {println!("taskman quit!");
+                             break;},
+                    Work(f) => tr.recv().send(Work(f)),
+                }
+            }
+        });
+        
+        let tm = TaskMan{tx:tx,fx:fx};
+        tm.spawn_n(n); //spawn atleast n workers
+        tm
+    }
+
+    pub fn spawn (&self) {
+        Worker::new(self.tx.clone());
+    }
+    pub fn spawn_n (&self, n:u8) {
+        for _ in range(0,n-1) {self.spawn();}
+    }
+
+    pub fn send (&self, f:DoTask) {
+        self.fx.send(f);
+    }
+}
+
+
+
 #[test]
 fn it_works() {
 }
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,13 @@
-use std::comm::{channel,Sender,Receiver};
+extern crate "rust-taskman" as taskman;
+use taskman::{TaskMan,Work,Quit};
 
-
-enum DoTask {
-    Quit,
-    Work(proc():Send), 
-}
-
-fn spawn_worker (tx:Sender<Sender<DoTask>>) {
-    spawn(proc() {
-        let (wx, wr) = channel::<DoTask>();
-        tx.send(wx.clone()); //send taskman our worker chan
-        //todo: arc add worker to taskman
-        loop { //wait on work
-            match wr.recv() {
-                Quit => {println!("quit!");
-                         break;},
-                Work(f) => {f();
-                            tx.send(wx.clone());}, //let taskman know we're waiting for more work
-            }
-        }
-        //todo: arc remove worker from taskman
-        });
-}
-
-fn spawn_taskman (fr:Receiver<DoTask>) -> Sender<Sender<DoTask>> {
-    let (tx,tr) = channel::<Sender<DoTask>>();
-    spawn(proc() {
-        loop {
-            match fr.recv() {
-                Quit => {println!("quit!");
-                         break;},
-                Work(f) => tr.recv().send(Work(f)),
-            }
-        }
-    });
-    tx
-}
-
-//fn send_work (f:DoTask,tch
 
 fn main (){
-    let w = Work(proc() {println!("hi!");});
-    let w2 = Quit;
-    let (fx, fr) = channel::<DoTask>();
+    let w = Work(proc() {println!("{}", 1i/0i);});
+    let w2 = Work(proc() {println!("hi!");});
     
-    let tx = spawn_taskman(fr);
-    spawn_worker(tx.clone());
-    fx.send(w);
-    fx.send(w2);
+    let tm = TaskMan::new(3);
+    tm.send(w);
+    tm.send(w2);
+    tm.send(Quit);
 }
